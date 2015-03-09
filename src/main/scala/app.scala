d5af1b99fed428c118338b0482cfaf5de14f873b
hxtpoe <hxtpoe@gmail.com>
2015-03-09 01:45:17 +0100

    search empty slots

--- src/main/scala/app.scala
+++ src/main/scala/app.scala
@@ -3,8 +3,11 @@ import com.github.nscala_time.time.Imports._
 
 object main extends App {
   DateTimeZone.setDefault(DateTimeZone.UTC)
-  
+
   val scheduler = new Scheduler
 
-  println(scheduler.search.length)
+  scheduler.run
+
+  // print slots in "best availability" order
+  scheduler.suitableSlots.toList.sortBy { -_._2 }.foreach(println)
 }
--- src/main/scala/scheduler/dataProvider.scala
+++ src/main/scala/scheduler/dataProvider.scala
@@ -8,13 +8,6 @@ package tool {
 
   class DataProvider {
 
-    val singleUserData = List(
-      new Interval(DateTime.parse("2015-03-09T08:00:00"), DateTime.parse("2015-03-09T16:00:00")),
-      new Interval(DateTime.parse("2015-03-10T08:00:00"), DateTime.parse("2015-03-10T16:00:00")),
-      new Interval(DateTime.parse("2015-03-11T08:00:00"), DateTime.parse("2015-03-11T16:00:00")),
-      new Interval(DateTime.parse("2015-03-12T08:00:00"), DateTime.parse("2015-03-12T16:00:00")),
-      new Interval(DateTime.parse("2015-03-13T08:00:00"), DateTime.parse("2015-03-13T16:00:00")))
-
     def getData() = List(
       row("John Smith", "Europe/Warsaw", List(
         new Interval(DateTime.parse("2015-03-09T08:00:00"), DateTime.parse("2015-03-09T16:00:00")),
@@ -30,12 +23,10 @@ package tool {
         new Interval(DateTime.parse("2015-03-12T08:00:00"), DateTime.parse("2015-03-12T16:00:00")),
         new Interval(DateTime.parse("2015-03-13T08:00:00"), DateTime.parse("2015-03-13T16:00:00")))),
 
-      row("Peter Sagan", "America/Havana", List(
-        new Interval(DateTime.parse("2015-03-09T08:00:00"), DateTime.parse("2015-03-09T16:00:00")),
-        new Interval(DateTime.parse("2015-03-10T08:00:00"), DateTime.parse("2015-03-10T16:00:00")),
-        new Interval(DateTime.parse("2015-03-11T11:00:00"), DateTime.parse("2015-03-11T12:00:00")), // !!
-        new Interval(DateTime.parse("2015-03-12T08:00:00"), DateTime.parse("2015-03-12T16:00:00")),
-        new Interval(DateTime.parse("2015-03-13T08:00:00"), DateTime.parse("2015-03-13T16:00:00")))))
+      row("Fryderyk Chopin", "America/Havana", List(
+        new Interval(DateTime.parse("2015-03-11T11:00:00"), DateTime.parse("2015-03-11T12:00:00")))), // !!
 
+      row("Peter Sagan", "America/Havana", List(
+        new Interval(DateTime.parse("2015-03-11T11:00:00"), DateTime.parse("2015-03-11T12:00:00"))))) // !!
   }
 }
